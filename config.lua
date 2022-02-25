88ed14a02c44ba200aeeb432e0de8daa6273d342
Jimmy Hogoboom <jimmy@differential.com>
2022-02-25 10:50:38 -0600

    Shade terminal darker

--- config.lua
+++ config.lua
@@ -65,10 +65,16 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.terminal.direction = 'tab'
+
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+
+lvim.builtin.terminal.active = true
+lvim.builtin.terminal.direction = 'tab'
+lvim.builtin.terminal.shade_terminals = true
+lvim.builtin.terminal.shade_filetypes = {}
+lvim.builtin.terminal.shading_factor = 1
+
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
