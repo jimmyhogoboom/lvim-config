9fd5804d82159f7b9fceb438a9d8a954e8de63ff
Jimmy Hogoboom <jimmy@differential.com>
2022-03-11 12:55:54 -0600

    Save a session when quitting

--- config.lua
+++ config.lua
@@ -25,7 +25,8 @@ vim.g["gruvbox_material_background"] = "hard"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<leader>q"] = ":mksession!<cr> | :q<cr>"
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
